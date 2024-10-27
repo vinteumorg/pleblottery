e78c428d2d71b980aa3d40fa5a951fcb6228c9a0
plebhash <plebhash@proton.me>
2024-10-27 12:03:33 -0300

    handle_tcp_stream

--- src/lib/sv1/service.rs
+++ src/lib/sv1/service.rs
@@ -1,4 +1,8 @@
+use std::net::SocketAddr;
+use futures::{StreamExt, FutureExt};
 use super::config::Sv1Config;
+
+const MAX_LINE_LENGTH: usize = 2_usize.pow(16);
 
 pub struct Sv1Service {
     listener: tokio::net::TcpListener,
@@ -23,10 +27,42 @@ impl Sv1Service {
         let handle = tokio::task::spawn(async move {
             while let Ok((stream, addr)) = self.listener.accept().await {
                 tracing::info!("established sv1 connection: {}", addr);
+
+                Self::handle_tcp_stream(stream, addr);
             }
             Ok(())
         });
 
         handle
+    }
+
+    fn handle_tcp_stream(mut stream: tokio::net::TcpStream, addr: std::net::SocketAddr) {
+        tokio::task::spawn(async move {
+            let (mut reader, mut writer) = stream.into_split();
+
+            let mut message_reader = tokio_util::codec::FramedRead::new(
+                reader,
+                tokio_util::codec::LinesCodec::new_with_max_length(MAX_LINE_LENGTH),
+            );
+
+            loop {
+                match message_reader.next().fuse().await {
+                    Some(r) => {
+                        match r {
+                            Ok(message) => {
+                                tracing::info!("received sv1 message from {} {}", addr, message);
+                            },
+                            Err(e) => {
+                                panic!("error reading TcpStream: {}", e);
+                            }
+                        }
+                    },
+                    None => {
+                        tracing::info!("closed sv1 connection: {}", addr);
+                        break;
+                    }
+                }
+            }
+        });
     }
 }
