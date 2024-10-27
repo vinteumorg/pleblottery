93f30593250e4d6e3071880c0cc65a75768abe12
plebhash <plebhash@proton.me>
2024-10-27 10:45:46 -0300

    bootstrap Sv1Service

--- src/lib/sv1/service.rs
+++ src/lib/sv1/service.rs
@@ -1,1 +1,32 @@
+use super::config::Sv1Config;
 
+pub struct Sv1Service {
+    listener: tokio::net::TcpListener,
+}
+
+impl Sv1Service {
+    pub async fn new(config: Sv1Config) -> anyhow::Result<Self> {
+        let listen_host = config.host;
+        let listen_port = config.port;
+        let listener = tokio::net::TcpListener::bind((listen_host.clone(), listen_port)).await?;
+
+        tracing::info!(
+            "listening for sv1 connections at: {}:{}",
+            listen_host,
+            listen_port
+        );
+
+        Ok(Self { listener })
+    }
+
+    pub fn serve(self) -> tokio::task::JoinHandle<anyhow::Result<()>> {
+        let handle = tokio::task::spawn(async move {
+            while let Ok((stream, addr)) = self.listener.accept().await {
+                tracing::info!("established sv1 connection: {}", addr);
+            }
+            Ok(())
+        });
+
+        handle
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,16 @@ async fn main() -> anyhow::Result<()> {
 
     let pleblottery_config = config::PlebLotteryConfig::new(args.config.clone())?;
 
+    // launch sv1 service
+    let sv1_service = lib::sv1::service::Sv1Service::new(pleblottery_config.sv1).await?;
+    let sv1_service_handle = sv1_service.serve();
+
+    // container for all service handles (todo: add sv2 service)
+    let service_handles = vec![sv1_service_handle];
+
+    // join all service handles
+    futures::future::join_all(service_handles).await;
+
     #[allow(unreachable_code)]
     Ok(())
 }
