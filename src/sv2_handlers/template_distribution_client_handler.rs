3729cc84067d65e1c1b527bf7680c3b6519912a4
Lucas Balieiro <github@lucasbalieiro.dev>
2025-05-15 20:33:00 -0400

    feat: redirect TDC calls to Mining Server and updates the web state

--- src/sv2_handlers/template_distribution_client_handler.rs
+++ src/sv2_handlers/template_distribution_client_handler.rs
@@ -1,16 +1,22 @@
 use anyhow::Result;
-use tower_stratum::client::service::request::RequestToSv2ClientError;
+use tower_stratum::client::service::request::{RequestToSv2Client, RequestToSv2ClientError};
 use tower_stratum::client::service::response::ResponseFromSv2Client;
 use tower_stratum::client::service::subprotocols::template_distribution::handler::Sv2TemplateDistributionClientHandler;
 use tower_stratum::roles_logic_sv2::template_distribution_sv2::{
     NewTemplate, RequestTransactionDataError, RequestTransactionDataSuccess, SetNewPrevHash,
 };
-
-use std::task::{Context, Poll};
+use tower_stratum::server::service::request::RequestToSv2Server;
+use tower_stratum::server::service::subprotocols::mining::request::RequestToSv2MiningServer;
 use tracing::info;
 
+use std::sync::Arc;
+use std::task::{Context, Poll};
+use tokio::sync::RwLock;
+
 #[derive(Debug, Clone, Default)]
-pub struct PlebLotteryTemplateDistributionClientHandler {}
+pub struct PlebLotteryTemplateDistributionClientHandler {
+    current_height: Arc<RwLock<u64>>,
+}
 
 impl Sv2TemplateDistributionClientHandler for PlebLotteryTemplateDistributionClientHandler {
     fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), RequestToSv2ClientError>> {
@@ -21,37 +27,53 @@ impl Sv2TemplateDistributionClientHandler for PlebLotteryTemplateDistributionCli
         &self,
         template: NewTemplate<'static>,
     ) -> Result<ResponseFromSv2Client<'static>, RequestToSv2ClientError> {
-        info!(
-            "Received NewTemplate message with template id {:?}",
-            template.template_id
+        let current_height = template.coinbase_prefix.to_vec().as_slice()[1..]
+            .iter()
+            .rev()
+            .fold(0, |acc, &byte| (acc << 8) | byte as u64)
+            - 1;
+
+        {
+            let mut height = self.current_height.write().await;
+            if current_height != *height {
+                *height = current_height;
+                info!("New Block Height: {}", current_height);
+            }
+        }
+
+        let response = ResponseFromSv2Client::TriggerNewRequest(
+            RequestToSv2Client::SendRequestToSiblingServerService(Box::new(
+                RequestToSv2Server::MiningTrigger(RequestToSv2MiningServer::NewTemplate(template)),
+            )),
         );
-        Ok(ResponseFromSv2Client::ToDo)
+        Ok(response)
     }
 
     async fn handle_set_new_prev_hash(
         &self,
         prev_hash: SetNewPrevHash<'static>,
     ) -> Result<ResponseFromSv2Client<'static>, RequestToSv2ClientError> {
-        info!(
-            "Received SetNewPrevHash message with prev hash {:?}",
-            prev_hash.prev_hash
+        let response = ResponseFromSv2Client::TriggerNewRequest(
+            RequestToSv2Client::SendRequestToSiblingServerService(Box::new(
+                RequestToSv2Server::MiningTrigger(RequestToSv2MiningServer::SetNewPrevHash(
+                    prev_hash,
+                )),
+            )),
         );
-        Ok(ResponseFromSv2Client::ToDo)
+        Ok(response)
     }
 
     async fn handle_request_transaction_data_success(
         &self,
         _transaction_data: RequestTransactionDataSuccess<'static>,
     ) -> Result<ResponseFromSv2Client<'static>, RequestToSv2ClientError> {
-        info!("Received RequestTransactionDataSuccess message");
-        Ok(ResponseFromSv2Client::ToDo)
+        unimplemented!("handle_request_transaction_data_success should not be called");
     }
 
     async fn handle_request_transaction_data_error(
         &self,
         _error: RequestTransactionDataError<'static>,
     ) -> Result<ResponseFromSv2Client<'static>, RequestToSv2ClientError> {
-        info!("Received RequestTransactionDataError message");
-        Ok(ResponseFromSv2Client::ToDo)
+        unimplemented!("handle_request_transaction_data_error should not be called");
     }
 }
--- src/web/server.rs
+++ src/web/server.rs
@@ -4,13 +4,17 @@ use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
 use crate::config::PlebLotteryWebConfig;
+use crate::state::SharedStateHandle;
 use crate::web::routes::{api::api_routes, html::html_routes};
 
-pub async fn start_web_server(web_config: &PlebLotteryWebConfig) -> Result<()> {
+pub async fn start_web_server(
+    web_config: &PlebLotteryWebConfig,
+    shared_state: SharedStateHandle,
+) -> Result<()> {
     let app = Router::new()
         .nest_service("/static", ServeDir::new("src/web/assets"))
         .merge(html_routes())
-        .merge(api_routes());
+        .merge(api_routes(shared_state));
 
     let addr = format!("0.0.0.0:{}", web_config.listening_port);
     let listener = TcpListener::bind(&addr).await?;
